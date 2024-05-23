3cc53ec7b0a1c1f33e37eb642266b2d44e22c078
Bob Gardner <rgardner@inworld.ai>
2024-05-23 15:54:09 -0700

    Adding scale to spatialbundle2d and raw

--- src/spatialbundle2d.rs
+++ src/spatialbundle2d.rs
@@ -5,17 +5,21 @@ use bevy::prelude::*;
 pub struct SpatialBundle2DRaw {
     pub position: Position2D,
     pub rotation: Rotation2D,
+    pub scale: Scale2D,
     pub draw_order: DrawOrder,
     pub r_prop: RotationPropogation,
     pub p_prop: PositionPropogation,
+    pub s_prop: ScalePropogation,
 }
 
 #[derive(Bundle, Default)]
 pub struct SpatialBundle2D {
     pub position: Position2D,
     pub rotation: Rotation2D,
+    pub scale: Scale2D,
     pub draw_order: DrawOrder,
     pub spatial: SpatialBundle,
     pub r_prop: RotationPropogation,
     pub p_prop: PositionPropogation,
+    pub s_prop: ScalePropogation,
 }
